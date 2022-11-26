30dc2b41c4d419add49bc376f8e00286b0047b88
c0zyb0y <no.phupha_st@tni.ac.th>
2022-11-26 11:21:24 +0700

    Workshop A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.status(200).json({
+    Fullname:'Phupha Noppakun'
+  })
 });
+router.get('/bio', function(req, res, next) {
+  res.status(200).json({
+    Fullname:'Phupha Noppakun',
+    nickname:'PooH',
+    hobby:'Sleep',
+    gitusername:'c0zyb0y'
+  })
+});
+
 
 module.exports = router;
